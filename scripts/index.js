a29ba8c697d379cbd75a3267203f010fe005a550
jdg006 <jdg006@lvc.edu>
2017-09-12 23:55:40 -0400

    checkpoint 7 complete

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,9 @@ var context = canvas.getContext('2d');
 var player = new Player('Joe');
 var computer = new Computer();
 var ball = new Ball();
+
 var animate = window.requestAnimationFrame ||
-              function(step) { window.setTimeout(step, 1000/60) };
+              function(step) { window.setTimeout(step, 1000/60)};
 
 var step = function (){
     score();
@@ -12,34 +13,43 @@ var step = function (){
     ball.move();
     computer.ai();
     render();
-    animate(step);
+    if (player.score != 11 && computer.score != 11){animate(step);}
+    else if(player.score == 11){
+      $('.landing').html("<h1> You Won</h1>" + "<h2>Refresh Page To Play Again</h2>");
+
+    }
+    else if(computer.score == 11){
+      $('.landing').html("<h1>Game Over. You Lost</h1>" + "<h2>Refresh Page To Play Again</h2>");
+    }
+
 }
 
 function Paddle(type){
   if (type == "computer"){
+    this.width = canvas.width/57.8;
+    this.height = canvas.height/3;
     this.x = 0;
-    this.y = 50;
-    this.width = 10;
-    this.height = 100;
-    this.speed = 0.5; //pixels per callback
+    this.y = (canvas.height/2) - (this.height/2);
+
+    this.speed = this.width/9; //pixels per callback
   }
   else {
-   this.x = 568;
-   this.y = 50;
-   this.width = 10;
-   this.height = 100;
-   this.speed = 10;
+   this.width = canvas.width/57.8;
+   this.height = canvas.height/3;
+   this.x = (canvas.width-this.width);
+   this.y = (canvas.height/2) - (this.height/2);
+   this.speed = this.width;
   }
 }
 
 function Ball(){
   this.x = canvas.width / 2;
   this.y = canvas.height / 2;
-  this.radius = 10;
+  this.radius = canvas.height/20;
   this.startAngle = 0;
   this.endAngle = 2 * Math.PI;
   this.counterClockwise = false;
-  this.speed = 1.3; //pixels per callback
+  this.speed = 1.6; //pixels per callback
   this.direction = "++";
 
 }
@@ -89,6 +99,7 @@ Ball.prototype.render = function(){
   context.stroke();
 };
 
+
 function score (){
   $('.score').html('<p>'+"Computer: " + computer.score + '  &nbsp; &nbsp; &nbsp; ' + player.name + ": " + player.score +"</p>")
 }
@@ -120,11 +131,11 @@ Ball.prototype.move = function(){
       if(this.direction == "++"){this.direction = "+-";}
       else {this.direction = "--";}
     }
-    else if (this.x - 10 <= 0){
+    else if (this.x - this.radius <= 0){
       player.score += 1;
       serve(ball);
     }
-    else if (this.x + 10 >= canvas.width){
+    else if (this.x + this.radius >= canvas.width){
       computer.score += 1;
       serve(ball);
     }
@@ -133,8 +144,8 @@ Ball.prototype.move = function(){
     else if ((this.x - this.radius) <= computer.paddle.width) {
             //collision
       if ((this.y + this.radius) >= computer.paddle.y && (this.y + this.radius) <= (computer.paddle.y + computer.paddle.height)){
-        if (this.direction == "-+"){this.direction = "++";}
-        else {this.direction = "+-";}
+        if (this.direction == "-+"){this.direction = "++"; ball.speed += .2;}
+        else {this.direction = "+-"; ball.speed += .1;}
       }
         else if ((this.y - this.radius) >= computer.paddle.y && (this.y - this.radius) <= (computer.paddle.y + computer.paddle.height)){
           if (this.direction == "-+"){this.direction = "++";}
@@ -188,12 +199,12 @@ window.onload = function(){
 window.addEventListener('keydown', function(event){
   var key = event.keyCode;
   if (key === 38 ){
-    if(player.paddle.y != 0){
+    if(player.paddle.y != 0 && player.paddle.y > 0){
       player.paddle.move("up");
     }
   }
   else if (key === 40){
-    if (player.paddle.y != 100){
+    if (player.paddle.y <= (canvas.height - player.paddle.height)-player.paddle.speed){
     player.paddle.move("down")
   }
 }
@@ -202,11 +213,11 @@ window.addEventListener('keydown', function(event){
 //AI
 Computer.prototype.ai = function (){
   if (ball.x < (canvas.width/4) && (ball.direction == "-+" || ball.direction == "--")){
-      var centerPaddle = this.paddle.y + 50;
-      if (centerPaddle > ball.y && centerPaddle - 50 != 0 ){
+      var centerPaddle = this.paddle.y + (this.paddle.height/2);
+      if (centerPaddle > ball.y && (this.paddle.y !=0 && this.paddle.y > 0)){
         this.paddle.move("up");
       }
-      else if (centerPaddle < ball.y && (centerPaddle + 50) != canvas.height ) {
+      else if (centerPaddle < ball.y && (centerPaddle + this.paddle.height) != canvas.height ) {
         this.paddle.move("down");
       }
   }
